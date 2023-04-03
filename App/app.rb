d4232ec804601274bc2011da387898d6e602c6ee
Viktorlehes <viktor.lehes@gmail.com>
2023-04-03 19:29:18 +0200

    Full documentation with yardoc

--- App/app.rb
+++ App/app.rb
@@ -6,24 +6,38 @@ require "rerun"
 require "net/http"
 require_relative "components/model.rb"
 
+include Model
+
 set :port, 3000
 
+# Generates a random key and sets session settings
 key = SecureRandom.hex(32)
 enable :sessions
 set :session_secret, key
 set :sessions, :expire_after => 2592000
 
 helpers do
+  # Adds a CSS file to the current page
+  #
+  # @param [String] path The path to the CSS file
+  # @return [nil] Returns nothing
   def link_css(path)
     css_path = "#{path}"
     @stylesheet_paths << css_path unless @stylesheet_paths.include?(css_path)
   end
 
+  # Gets the value of a key in the @values hash
+  #
+  # @param [Symbol] key The key to retrieve the value for
+  # @return [String] The value of the key or an empty string if it doesn't exist
   def value(key)
     @values&.fetch(key, "")
   end
 end
 
+# Redirects unauthenticated users to the login page for protected and admin routes
+#
+# @return [nil] Returns nothing
 before do
   @stylesheet_paths = []
   protectedRoutes = ["/featured"]
@@ -40,8 +54,14 @@ before do
   end
 end
 
+# Redirects to login page
+#
+# @return [Slim::Template] The rendered login page template
 get("/") { redirect("/auth") }
 
+# Renders login page template
+#
+# @return [Slim::Template] The rendered login page template
 get("/auth") do
   if session[:error]
     error = session[:error]
@@ -62,15 +82,26 @@ get("/auth") do
   slim(:"auth/auth", :layout => :"layouts/layout_registration", locals: { error: error })
 end
 
+# Registers a new user
+#
+# @param [String] username_register The username of the user to register
+# @param [String] password_register The password of the user to register
+# @param [String] passwordConfirm The confirmation password of the user to register
+# @return [nil] Returns nothing
 post("/register") do
   username, password, passwordConfirm = params[:username_register].downcase, params[:password_register], params[:"passwordConfirm"]
   status = createUser(username, password, passwordConfirm)
 
   session[:values] = { username_register: params[:username_register], password_register: params[:password_register], password_confirm: params[:passwordConfirm] }
 
-  redirect("auth")
+  redirect("auth?login=true")
 end
 
+# Logs in a user
+#
+# @param [String] username The username of the user to log in
+# @param [String] password The password of the user to log in
+# @return [nil] Returns nothing
 post("/login") do
   username = params[:username].downcase
   password = params[:password]
@@ -86,11 +117,17 @@ post("/login") do
   end
 end
 
+# Logs out a user
+#
+# @return [nil] Returns nothing
 get("/logout") do
   session.clear
   redirect("/auth")
 end
 
+# Renders the featured page template
+#
+# @return [Slim::Template] The rendered featured page template
 get("/featured") do
   standardDashboardData = getStandardWeatherData()
   optimizedStandardDashboardData = optimizeTempforArray(standardDashboardData)
@@ -101,12 +138,19 @@ get("/featured") do
   slim(:"main/featured", :layout => :"layouts/layout_main", locals: { dashboard_data: optimizedStandardDashboardData, selectedCityData: selectedCityData, role: role })
 end
 
+# Handles the selected city on the featured page
+#
+# @param [String] selected The selected city
+# @return [nil] Returns nothing
 post("/featured-selected-city") do
   selectedCity = params[:selected].downcase
   session[:selectedCity] = selectedCity
   redirect("/featured")
 end
 
+# Renders the dashboard page template
+#
+# @return [Slim::Template] The rendered dashboard page template
 get("/dashboard") do
   if params["city"]
     lon, lat, status = getCityCordinatesFromSearch(params["city"])
@@ -132,19 +176,27 @@ get("/dashboard") do
   slim(:"main/dashboard", :layout => :"layouts/layout_main", locals: { dashboardWeahterData: dashboardWeatherData, savedLocations: saved_locations, role: role })
 end
 
+# Handles the searched city on the dashboard page
+#
+# @param [String] searchedCity The searched city
+# @return [nil] Returns nothing
 post("/dashboard") do
   session["searchedCity"] = params["searchedCity"].downcase
   redirect("/dashboard?city=#{session["searchedCity"]}")
 end
 
+# Handles saving a city on the dashboard page
+#
+# @param [String] city-save The city to save
+# @return [nil] Returns nothing
 post("/dashboard-save-city") do
   saved_city = params["city-save"].downcase
 
   db = connectToDb()
 
   city_data = db.execute("INSERT INTO location(name) VALUES($1) 
-                         ON CONFLICT(name) DO UPDATE SET name=excluded.name 
-                         RETURNING id", saved_city)
+                          ON CONFLICT(name) DO UPDATE SET name=excluded.name 
+                          RETURNING id", saved_city)
 
   city_id = city_data[0]["id"]
 
@@ -153,6 +205,11 @@ post("/dashboard-save-city") do
   redirect("/dashboard?city=#{saved_city}")
 end
 
+# Handles selecting or deleting a city on the dashboard page
+#
+# @param [String] selected-city-select The selected city to display
+# @param [String] selected-city-delete The selected city to delete
+# @return [nil] Returns nothing
 post("/dashboard-selected-city") do
   intent_select = params["selected-city-select"]
   intent_delete = params["selected-city-delete"]
@@ -168,9 +225,11 @@ post("/dashboard-selected-city") do
 
     db = connectToDb()
 
+    # Gets the location ID for the selected city
     location_id_data = db.execute("SELECT id FROM location WHERE name = $1", selected_city)
     location_id = location_id_data[0]["id"]
 
+    # Deletes the city from the user's saved locations
     db.prepare("DELETE FROM ulr WHERE locationId = $1 AND userId = $2").execute(location_id, session[:loggedIn])
 
     redirect("/dashboard?city=gothenburg")
@@ -179,6 +238,9 @@ post("/dashboard-selected-city") do
   redirect("/dashboard")
 end
 
+# Renders the overview page template
+#
+# @return [Slim::Template] The rendered overview page template
 get("/overview") do
   role = session[:admin]
 
@@ -187,6 +249,10 @@ get("/overview") do
   slim(:"main/overview", :layout => :"layouts/layout_main", locals: { role: role, users: users })
 end
 
+# Handles editing, deleting, and updating a user on the overview page
+#
+# @param [String] userId The ID of the user to edit or delete
+# @return [nil] Returns nothing
 post("/overview/edit/:userId") do |userId|
   if params["username-edit"]
     new_username = params["username-edit"].downcase.strip
@@ -213,6 +279,10 @@ post("/overview/edit/:userId") do |userId|
   redirect("/overview")
 end
 
+# Deletes a user from the database
+#
+# @param [String] userId The ID of the user to delete
+# @return [nil] Returns nothing
 post("/overview/delete/:userId") do |userId|
   if session[:admin]
     db = connectToDb()
--- App/components/model.rb
+++ App/components/model.rb
@@ -4,187 +4,273 @@ require "sqlite3"
 require "bcrypt"
 require "net/http"
 
-def connectToDb()
-  db = SQLite3::Database.new("db/new.db")
-  db.results_as_hash = true
-  return db
-end
-
-def getUserByUsername(username)
-  db = connectToDb()
-  user = db.execute("SELECT * FROM users WHERE username=?", username).first || nil
-  return user
-end
-
-def createUser(username, password, passwordConfirm)
-  db = connectToDb()
-  user = getUserByUsername(username)
-  status = 200
-  standardRole = "standard"
-  session.clear
-
-  if username.empty? or password.empty?
-    session.delete("error")
-    session[:error] = "Please fill in all fields"
-    return status = 400
+# Module Model
+module Model
+  # Connect to SQLite3 database and return database object
+  #
+  # @return [SQLite3::Database] SQLite3 database object
+  def connectToDb()
+    db = SQLite3::Database.new("db/new.db")
+    db.results_as_hash = true
+    return db
   end
 
-  if user
-    session.delete("error")
-    session[:error] = "User already exists"
-    return status = 400
+  # Get user object from SQLite3 database by username
+  #
+  # @param [String] username The username of the user
+  # @return [Hash, nil] A hash representing the user, or nil if not found
+  def getUserByUsername(username)
+    db = connectToDb()
+    user = db.execute("SELECT * FROM users WHERE username=?", username).first || nil
+    return user
   end
 
-  if password != passwordConfirm
-    session.delete("error")
-    session[:error] = "Confirmation password is incorrect"
-    return status = 400
-  end
+  # Create a new user in SQLite3 database
+  #
+  # @param [String] username The username of the user
+  # @param [String] password The password of the user
+  # @param [String] passwordConfirm The confirmation password of the user
+  # @return [Integer] HTTP status code indicating success or error
+  def createUser(username, password, passwordConfirm)
+    db = connectToDb()
+    user = getUserByUsername(username)
+    status = 200
+    standardRole = "standard"
+    session.clear
 
-  passwordDigest = BCrypt::Password.create(password)
-  db.execute(
-    "INSERT INTO users (username, passwordDigest, auth) VALUES (?, ?, ?);",
-    [username.downcase, passwordDigest, standardRole]
-  )
-  return status
-end
-
-def loginUser(username, password)
-  db = connectToDb()
-  user = getUserByUsername(username)
-  status = 200
-  session.clear
-
-  if password == "" || username == ""
-    session[:error] = "please fill in all required fields"
-    return status = 400
-  end
-
-  if !user
-    session[:error] = "wrong username or password"
-    return status = 400
-  end
-
-  passwordDigest = user["passwordDigest"]
-
-  if BCrypt::Password.new(passwordDigest) != password
-    session[:error] = "wrong username or password"
-    return status = 400
-  end
-
-  # token = BCrypt::Password.create(user["userId"])
-
-  if user["auth"] == "admin"
-    session[:admin] = true
-  else
-    session[:admin] = false
-  end
-
-  session[:loggedIn] = user["userId"]
-
-  return status
-end
-
-def getCityCordinatesFromSearch(cityName)
-  status = 200
-
-  if cityName.include?(" ")
-    searchedCity = cityName.split(" ")[0]
-  else
-    searchedCity = cityName
-  end
-
-  uri = URI("http://api.openweathermap.org/geo/1.0/direct?q=#{searchedCity}&limit=5&appid=9e23271195b29b37c3bac4c4457487cf")
-  response = Net::HTTP.get(uri)
-  weatherData = JSON.parse(response)
-
-  p weatherData
-
-  if weatherData == nil || weatherData == []
-    return latitude = nil, longitude = nil, status = 400
-  end
-  latitude = weatherData[1]["lat"]
-  longitude = weatherData[1]["lon"]
-  return latitude, longitude, status
-end
-
-def getWeatherDataByCordinates(lon, lat)
-  uri = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{lon}&lon=#{lat}&units=metric&appid=8ff21487fbf05de0ac8a7e46b7643b72")
-
-  response = Net::HTTP.get(uri)
-
-  weatherData = JSON.parse(response)
-
-  return weatherData
-end
-
-def getStandardWeatherData()
-  db = connectToDb()
-
-  standard_dashboard_cities = db.execute(
-    "SELECT name, lon, lat FROM location"
-  )
-
-  standard_dashboard_data = []
-
-  standard_dashboard_cities.each do |city|
-    if city["name"] == "sundsvall kommun" or city["name"] == "gothenburg" or city["name"] == "lund municipality" or city["name"] == "stockholm" or city["name"] == "halmstad" or city["name"] == "karlstad"
-      uri = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{city["lon"]}&lon=#{city["lat"]}&units=metric&appid=8ff21487fbf05de0ac8a7e46b7643b72")
-
-      response = Net::HTTP.get(uri)
-
-      weatherData = JSON.parse(response)
-
-      standard_dashboard_data << weatherData
+    if username.empty? or password.empty?
+      session.delete("error")
+      session[:error] = "Please fill in all fields"
+      return status = 400
     end
+
+    if user
+      session.delete("error")
+      session[:error] = "User already exists"
+      return status = 400
+    end
+
+    if password != passwordConfirm
+      session.delete("error")
+      session[:error] = "Confirmation password is incorrect"
+      return status = 400
+    end
+
+    passwordDigest = BCrypt::Password.create(password)
+    db.execute(
+      "INSERT INTO users (username, passwordDigest, auth) VALUES (?, ?, ?);",
+      [username.downcase, passwordDigest, standardRole]
+    )
+    return status
   end
 
-  return standard_dashboard_data
-end
+  # Verify user credentials and login
+  #
+  # @param [String] username The username of the user
+  # @param [String] password The password of the user
+  # @return [Integer] HTTP status code indicating success or error
+  def loginUser(username, password)
+    db = connectToDb()
+    user = getUserByUsername(username)
+    status = 200
+    session.clear
 
-def getGothenburgWeather()
-  weatherData = getWeatherDataByCordinates(40.9277324, -100.1619896)
-  return weatherData
-end
+    if password == "" || username == ""
+      session[:error] = "please fill in all required fields"
+      return status = 400
+    end
 
-def optimizeTempforArray(standardDashboardData)
-  roundedDashboardData = standardDashboardData
+    if !user
+      session[:error] = "wrong username or password"
+      return status = 400
+    end
 
-  roundedDashboardData.each do |data|
+    passwordDigest = user["passwordDigest"]
+
+    if BCrypt::Password.new(passwordDigest) != password
+      session[:error] = "wrong username or password"
+      return status = 400
+    end
+
+    # token = BCrypt::Password.create(user["userId"])
+
+    if user["auth"] == "admin"
+      session[:admin] = true
+    else
+      session[:admin] = false
+    end
+
+    session[:loggedIn] = user["userId"]
+
+    return status
+  end
+
+  # Get the latitude and longitude of a city from OpenWeatherMap API
+  #
+  # @param [String] cityName The name of the city to search for
+  # @return [Float, Float, Integer] The latitude and longitude of the city, or nil if not found, and HTTP status code indicating success or error
+  def getCityCordinatesFromSearch(cityName)
+    status = 200
+
+    if cityName.include?(" ")
+      searchedCity = cityName.split(" ")[0]
+    else
+      searchedCity = cityName
+    end
+
+    uri = URI("http://api.openweathermap.org/geo/1.0/direct?q=#{searchedCity}&limit=5&appid=9e23271195b29b37c3bac4c4457487cf")
+    response = Net::HTTP.get(uri)
+    weatherData = JSON.parse(response)
+
+    p weatherData
+
+    if weatherData == nil || weatherData == []
+      return latitude = nil, longitude = nil, status = 400
+    end
+    latitude = weatherData[1]["lat"]
+    longitude = weatherData[1]["lon"]
+    return latitude, longitude, status
+  end
+
+  # Get weather data for a location from OpenWeatherMap API
+  #
+  # @param [Float] lon The longitude of the location
+  # @param [Float] lat The latitude of the location
+  # @return [Hash] A hash representing the weather data for the location
+  def getWeatherDataByCordinates(lon, lat)
+    uri = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{lon}&lon=#{lat}&units=metric&appid=8ff21487fbf05de0ac8a7e46b7643b72")
+
+    response = Net::HTTP.get(uri)
+
+    weatherData = JSON.parse(response)
+
+    return weatherData
+  end
+
+  # Get weather data for a list of cities from SQLite3 database
+  #
+  # @return [Array<Hash>] An array of hashes representing the weather data for each city
+  def getStandardWeatherData()
+    db = connectToDb()
+
+    standard_dashboard_cities = db.execute(
+      "SELECT name, lon, lat FROM location"
+    )
+
+    standard_dashboard_data = []
+
+    standard_dashboard_cities.each do |city|
+      if city["name"] == "sundsvall kommun" or city["name"] == "gothenburg" or city["name"] == "lund municipality" or city["name"] == "stockholm" or city["name"] == "halmstad" or city["name"] == "karlstad"
+        uri = URI("https://api.openweathermap.org/data/2.5/weather?lat=#{city["lon"]}&lon=#{city["lat"]}&units=metric&appid=8ff21487fbf05de0ac8a7e46b7643b72")
+
+        response = Net::HTTP.get(uri)
+
+        weatherData = JSON.parse(response)
+
+        standard_dashboard_data << weatherData
+      end
+    end
+
+    return standard_dashboard_data
+  end
+
+  # Get weather data for Gothenburg, Sweden from OpenWeatherMap API
+  #
+  # @return [Hash] A hash representing the weather data for Gothenburg, Sweden
+  def getGothenburgWeather()
+    weatherData = getWeatherDataByCordinates(40.9277324, -100.1619896)
+    return weatherData
+  end
+
+  def optimizeTempforArray(standardDashboardData)
+    roundedDashboardData = standardDashboardData
+
+    roundedDashboardData.each do |data|
+      realTemp = data["main"]["temp"].round(0)
+      feelsLike = data["main"]["feels_like"].round(0)
+      data["main"]["temp"] = realTemp
+      data["main"]["feels_like"] = feelsLike
+    end
+    return roundedDashboardData
+  end
+
+  # Round the temperature values in an array of weather data
+  #
+  # @param [Array<Hash>] standardDashboardData An array of hashes representing weather data for each location
+  # @return [Array<Hash>] The same array of weather data, with temperature values rounded
+  def optimizeTempforArray(standardDashboardData)
+    roundedDashboardData = standardDashboardData
+
+    roundedDashboardData.each do |data|
+      realTemp = data["main"]["temp"].round(0)
+      feelsLike = data["main"]["feels_like"].round(0)
+      data["main"]["temp"] = realTemp
+      data["main"]["feels_like"] = feelsLike
+    end
+    return roundedDashboardData
+  end
+
+  # Round the temperature values in a single hash of weather data
+  #
+  # @param [Hash] data A hash representing the weather data for a single location
+  # @return [Hash] The same hash of weather data, with temperature values rounded
+  def optimizeTempForHash(data)
     realTemp = data["main"]["temp"].round(0)
     feelsLike = data["main"]["feels_like"].round(0)
     data["main"]["temp"] = realTemp
     data["main"]["feels_like"] = feelsLike
+    return data
   end
-  return roundedDashboardData
-end
 
-def optimizeTempForHash(data)
-  realTemp = data["main"]["temp"].round(0)
-  feelsLike = data["main"]["feels_like"].round(0)
-  data["main"]["temp"] = realTemp
-  data["main"]["feels_like"] = feelsLike
-  return data
-end
+  # Get a list of all users from SQLite3 database
+  #
+  # @return [Array<Hash>] An array of hashes representing each user
+  def getAllUsers()
+    db = connectToDb()
+    users = db.execute("SELECT * FROM users")
+    return users
+  end
 
-def getAllUsers()
-  db = connectToDb()
-  users = db.execute("SELECT * FROM users")
-  return users
-end
+  def updateRole(userId, new_role)
+    db = connectToDb()
 
-def updateRole(userId, new_role)
-  db = connectToDb()
+    db.execute("UPDATE users SET auth = ? WHERE userId = ?", [new_role, userId])
 
-  db.execute("UPDATE users SET auth = ? WHERE userId = ?", [new_role, userId])
+    return nil
+  end
 
-  return nil
-end
+  def updateUsername(userId, new_username)
+    db = connectToDb()
 
-def updateUsername(userId, new_username)
-  db = connectToDb()
+    db.execute("UPDATE users SET username = ? WHERE userId = ?", [new_username, userId])
 
-  db.execute("UPDATE users SET username = ? WHERE userId = ?", [new_username, userId])
+    return nil
+  end
 
-  return nil
+  # Update the role of
+  # Update the role of a user in SQLite3 database
+  #
+  # @param [Integer] userId The ID of the user to update
+  # @param [String] new_role The new role of the user
+  # @return [nil] Returns nil
+  def updateRole(userId, new_role)
+    db = connectToDb()
+
+    db.execute("UPDATE users SET auth = ? WHERE userId = ?", [new_role, userId])
+
+    return nil
+  end
+
+  # Update the username of a user in SQLite3 database
+  #
+  # @param [Integer] userId The ID of the user to update
+  # @param [String] new_username The new username of the user
+  # @return [nil] Returns nil
+  def updateUsername(userId, new_username)
+    db = connectToDb()
+
+    db.execute("UPDATE users SET username = ? WHERE userId = ?", [new_username, userId])
+
+    return nil
+  end
 end
